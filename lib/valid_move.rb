f6379a19b77f8d198a5e446fdc66ccfff40d5fcc
Ana Bebic <anabebic.x@gmail.com>
2018-06-12 08:21:06 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,12 @@
-
-def valid_move?(number_entered)
-  number_entered.between?(0, 8) && !(position_taken?(board, number_entered))
-end
-
 def position_taken?(board, index)
- !(board[index] == " " || board[index] == "" || board[index] == nil)
+  !(board[index].nil? || board[index] == " ")
 end
 
-puts valid_move?(number_entered)
+# Checks that the move entered is a valid move, by checking that the position is not taken and that the number of
+def valid_move?(board, index)
+  if position_taken?(board,index) || index < 0 || index > 8
+    return false
+  elsif !position_taken?(board, index)
+    return true
+  end
+end
