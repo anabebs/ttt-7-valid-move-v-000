4b171df632121e6d6ec006b428ab65c01a9d45e6
Ana Bebic <anabebic.x@gmail.com>
2018-06-12 08:21:18 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,6 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Checks that the move entered is a valid move, by checking that the position is not taken and that the number of
 def valid_move?(board, index)
   if position_taken?(board,index) || index < 0 || index > 8
     return false
